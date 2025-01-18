8f6e3a9a96c93c66015c2c4a2a8ef2dad695d2f5
budionoaryawisanto <budionoarya.w@gmail.com>
2025-01-18 17:21:20 +0700

    update

--- src/components/DetailCards.js
+++ src/components/DetailCards.js
@@ -2,6 +2,7 @@ import React from 'react'
 import api from '../models/api'
 import '../style/components/DetailCards.css'
 import { useLocation } from 'react-router-dom'
+import { FormatRupiah } from "@arismun/format-rupiah";
 import Navbar from './Navbar'
 import Footer from './Footer'
 
@@ -16,6 +17,7 @@ const DetailCards = () => {
         <div className="container-detail-title">
             <p className='detail-card-title'>{data.title}</p>
             <p className='detail-card-location'>{data.location}</p>
+        <p className='detail-card-price'><FormatRupiah value={data.regularPrice}/></p>
         </div>
         <Footer/>
     </div>
